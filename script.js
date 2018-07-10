5d6162fe6322f0b5723cf7ec2649a71330d902f3
Ryan Stubbs <rastubbs86@gmail.com>
2018-07-09 20:02:22 -0500

    updated scripting, URl

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var fetch = require('node-fetch')
+const fetch = require('node-fetch')
+
+let url = 'https://jsonplaceholder.typicode.com/photos?albumId=' + process.argv
 
 
 fetch('https://jsonplaceholder.typicode.com/photos?albumId=3')
 .then((response) => response.json())
-.then(function(data) {
-	console.log(data.map((photo) => `[${photo.id}] ${photo.title}`));
+.then((data) => {
+	console.log(data.map((photo) => `id[${photo.id}] ${photo.title}`));
 
 	
 })
 
 
 
-.catch(function(error) {
+.catch((error) => {
     console.log(error);
  });   
