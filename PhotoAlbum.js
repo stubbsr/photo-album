840ea635f6ef926a4a30edb56361221cffa8511b
Ryan Stubbs <rastubbs86@gmail.com>
2018-08-20 16:12:08 -0500

    updating styling

--- PhotoAlbum.js
+++ PhotoAlbum.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch')
 
 export const photoAlbum = (url) => {
-	fetch(url)
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data.map((photo) => `id[${photo.id}] ${photo.title}`));
-	})
-	.catch((error) => {
-   	console.log(error);
-	});
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.map((photo) => `id[${photo.id}] ${photo.title}`));
+    })
+    .catch((error) => {
+    console.log(error);
+    });
 };   
 
--- test/photoAlbum.test.js
+++ test/photoAlbum.test.js
@@ -6,19 +6,19 @@ const expect = chai.expect;
 
 
 describe('photo album', () => {
-	it('should give a response', () => {
-		let url = BASE_URL + "3";
-		return photoAlbum(url)
-			expect(photoAlbum.ok).to.be.true;
-		})
-	});
-	it('should return a JSON object', () => {
-		let url = BASE_URL + "1";
-		return photoAlbum(url)
-			expect(photoAlbum).to.be.an('array');
-		});
-	it('should fail with error on bad url', () => {
-		let url = BASE_URL + "2222"
-		return photoAlbum(url) 
-			expect(photoAlbum).to.eventually.be.rejected;
-	});
+   it('should give a response', () => {
+      let url = BASE_URL + "3";
+      return photoAlbum(url)
+         expect(photoAlbum.ok).to.be.true;
+      })
+   });
+   it('should return a JSON object', () => {
+      let url = BASE_URL + "1";
+      return photoAlbum(url)
+         expect(photoAlbum).to.be.an('array');
+      });
+   it('should fail with error on bad url', () => {
+      let url = BASE_URL + "2222"
+      return photoAlbum(url) 
+         expect(photoAlbum).to.eventually.be.rejected;
+   });
