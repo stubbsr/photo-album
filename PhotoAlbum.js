3f7dc42ddb7cffd8f09942830b905495e943d341
Ryan Stubbs <rastubbs86@gmail.com>
2018-08-20 16:04:08 -0500

    fixing styling

--- PhotoAlbum.js
+++ PhotoAlbum.js
@@ -4,10 +4,10 @@ export const photoAlbum = (url) => {
 	fetch(url)
 	.then((response) => response.json())
 	.then((data) => {
-	console.log(data.map((photo) => `id[${photo.id}] ${photo.title}`));
+		console.log(data.map((photo) => `id[${photo.id}] ${photo.title}`));
 	})
 	.catch((error) => {
-    console.log(error);
+   	console.log(error);
 	});
 };   
 
